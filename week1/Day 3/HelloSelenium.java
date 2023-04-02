9da3e7e24b897b6d7e4a414224bcfba6aa56c78e
connectSachithra <129304532+connectSachithra@users.noreply.github.com>
2023-04-02 19:08:22 +0530

    Add files via upload

---
+++ week1/Day 3/HelloSelenium.java
@@ -0,0 +1,17 @@
+package selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		
+		
+	}
+
+}
