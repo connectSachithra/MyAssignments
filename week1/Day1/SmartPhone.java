7f1f890f1ed8aee424e7754b0c7fac8429ca1e8f
connectSachithra <129304532+connectSachithra@users.noreply.github.com>
2023-04-04 21:15:15 +0530

    Add files via upload

---
+++ week1/Day1/SmartPhone.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class SmartPhone {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//className objectName=new classname();
+		//SmartPhone mobileClass=new  SmartPhone();
+		
+		
+        Iphone mobileClass=new Iphone();
+
+
+		
+		//ObjectName.method();
+		mobileClass.makeCall();
+		mobileClass.model();
+		
+	}
+
+}
